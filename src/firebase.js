9d3a6bb010f757c61ed8761e9ea429cdfc5a2fd6
ruhird09 <81101482+ruhird09@users.noreply.github.com>
2021-03-21 22:32:04 +0530

    src
    source folder uploaded

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCz52scVptfgbCwr48iAJMuFOw9VKqUZsw",
+    authDomain: "whatsapp-mern-1b093.firebaseapp.com",
+    projectId: "whatsapp-mern-1b093",
+    storageBucket: "whatsapp-mern-1b093.appspot.com",
+    messagingSenderId: "786215669130",
+    appId: "1:786215669130:web:64ed29beaac7755ceacc73"
+  };
